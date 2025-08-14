c8546c28364e5e148672029ea30fae5335d3d924
bbtufty <bbtufty@gmail.com>
2025-08-14 12:11:12 +0100

    Add support for RuTracker
    - Add support for RuTracker

--- seadexarr/modules/torrent.py
+++ seadexarr/modules/torrent.py
@@ -1,10 +1,11 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlencode
 
 import pynyaa
 import requests
 from bs4 import BeautifulSoup
 
 ANIMETOSHO_FEED_URL = "https://animetosho.org/feed/json"
+RUTRACKER_MAGNET_ANNOUNCE = "http://bt2.t-ru.org/ann?magnet"
 
 
 def get_nyaa_url(url):
@@ -55,5 +56,33 @@ def get_animetosho_url(url):
         link = i.get("link", None)
         if link == url:
             parsed_url = i.get("torrent_url", None)
+
+    return parsed_url
+
+
+def get_rutracker_url(
+    url,
+    torrent_hash,
+):
+    """Get RuTracker torrent link from URL
+
+    Args:
+        url (str): URL to get RuTracker torrent link
+        torrent_hash (str): Torrent hash
+    """
+
+    # Pull the torrent title from souping the URL
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "lxml")
+    main_title = soup.find("h1", attrs={"class": "maintitle"})
+    torrent_title = main_title.text
+
+    params = {
+        "xt": f"urn:btih:{torrent_hash}",
+        "tr": RUTRACKER_MAGNET_ANNOUNCE,
+        "dn": torrent_title,
+    }
+    url_encoded = urlencode(params)
+    parsed_url = f"magnet:?{url_encoded}"
 
     return parsed_url
